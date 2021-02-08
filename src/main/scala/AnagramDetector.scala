863649a3c188a619b51394b6fc5165663b82459f
Eduard <edusrvt@gmail.com>
2021-02-08 11:24:59 +0100

    Pass the test

--- src/main/scala/AnagramDetector.scala
+++ src/main/scala/AnagramDetector.scala
@@ -1,7 +1,17 @@
 package org.anagrams.application
 
 object AnagramDetector extends App {
-  def isAnagram(wordA: String, wordB: String): Boolean = {
-    return false;
+  def isAnagram(textA: String, textB: String): Boolean = {
+    val a: String = formatText(textA).sortWith(_ < _)
+    val b: String = formatText(textB).sortWith(_ < _)
+    if (a == b) {
+      true;
+    } else {
+      false;
+    }
+  }
+
+  def formatText(text: String): String = {
+    text.toLowerCase().replace(" ", "")
   }
 }
--- src/test/scala/AnagramDetectorTest.scala
+++ src/test/scala/AnagramDetectorTest.scala
@@ -4,6 +4,6 @@ import org.scalatest.FunSuite
 
 class AnagramDetectorTest extends FunSuite {
   test("AnagramDetector.isAnagram") {
-    assert(AnagramDetector.isAnagram("New York Times", "Monkeys Write") === true)
+    assert(AnagramDetector.isAnagram("New York Times", "Monkeys Write"))
   }
 }
